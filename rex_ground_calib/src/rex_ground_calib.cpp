0eb0c171f3be1645f85b15b5562dafb40d0d7620
Manu Lange <manu.lange@rexbionics.com>
2016-10-18 09:32:45 +1300

    testing stash

--- rex_ground_calib/src/rex_ground_calib.cpp
+++ rex_ground_calib/src/rex_ground_calib.cpp
@@ -22,6 +22,7 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/uniform_sampling.h>
 
 #include <ros/ros.h>
 #include <std_srvs/Empty.h>
